305e8f832dac137f308da3c3d2eda047e466eb27
Ricardo Evans <Ricardo.Evans.T0130955@edu.atec.pt>
2025-11-17 18:20:58 +0000

    Add project files.

---
+++ LauGardensApi/LauGardensApi/LauGardensApi/Controllers/PlantasController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LauGardensApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantasController : ControllerBase
+    {
+
+        public PlantasController() { }
+    }
+}
---
+++ LauGardensApi/LauGardensApi/Models/Utilizadores.cs
@@ -0,0 +1,15 @@
+﻿namespace LauGardensApi.Models
+{
+    public class Utilizadores
+    {
+        int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public int? Idade { get; set; }
+
+        public int? Nif { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
